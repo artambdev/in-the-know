c0cca57c1b685049b0ff7278e9098d5b98b56ff9
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-03 18:47:42 +0000

    Create basic post model in list app

--- list/models.py
+++ list/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    #slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posts"
+    )
+    reply_to = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="replies"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    hidden = models.BooleanField()
+    ever_edited = False
+
+    def __str__(self):
+        return f"{self.content[:50]}"
