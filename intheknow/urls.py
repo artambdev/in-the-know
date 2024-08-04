73e4c2673b5926d3ef91f60dafa164951eabcad5
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-04 17:42:54 +0000

    Adjust urls.py to prepare for addition of templates

--- intheknow/urls.py
+++ intheknow/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from list.views import create_list
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', create_list, name="main"),
+    path("", include("list.urls"), name="list-urls"),
 ]
