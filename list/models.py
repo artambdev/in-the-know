a08bf5976e7fafb6ba6c883ad81e7407c81de708
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-09 20:52:51 +0000

    Add slug field to posts

--- list/models.py
+++ list/models.py
@@ -8,8 +8,9 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="posts"
     )
+    slug = models.SlugField(max_length=200, default=None, null=True)
     reply_to = models.ForeignKey(
-        "self", on_delete=models.CASCADE, related_name="replies", default=None, blank=True
+        "self", on_delete=models.CASCADE, related_name="replies", null=True, blank=True
     )
     content = models.TextField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -17,12 +18,11 @@ class Post(models.Model):
     hidden = models.BooleanField()
     ever_edited = False
 
-    def save(self, *args, **kwargs):
-        slug = f"{self.author}-{self.created_on}-{self.content[:10]}"
-        super().save(*args, **kwargs)
-
     def Meta(self):
         ordering = ["-created_on"]
 
     def get_excerpt(self):
         return f"{self.content[:50]}"
+    
+    def __str__(self):
+        return f'"{self.content[:20]}..." | written by {self.author}'
