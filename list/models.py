d5a84c61535dc56e21a31247bd42f9757f661812
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-09 19:36:18 +0000

    Generate post slug based on author, creation date and content

--- list/models.py
+++ list/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     #title = models.CharField(max_length=200, unique=True)
-    #slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="posts"
     )
@@ -17,6 +16,8 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     hidden = models.BooleanField()
     ever_edited = False
+
+    slug = f"{author}-{created_on}-{content[:10]}"
 
     def Meta(self):
         ordering = ["-created_on"]
