2fec8dec1f65e4d90a0ea7af52ae1fc8811988af
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-09 19:31:11 +0000

    Rearrange post card and model to remove title

--- list/models.py
+++ list/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    #title = models.CharField(max_length=200, unique=True)
     #slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="posts"
@@ -12,7 +12,7 @@ class Post(models.Model):
     reply_to = models.ForeignKey(
         "self", on_delete=models.CASCADE, related_name="replies"
     )
-    content = models.TextField()
+    content = models.TextField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     hidden = models.BooleanField()
