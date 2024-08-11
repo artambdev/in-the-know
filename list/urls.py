a7f4649679ac34b4b7809bfd57454f8f440fcccd
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-11 19:28:39 +0000

    Add url pattern for post deletion to list urls.py

--- list/urls.py
+++ list/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.MainPage.as_view(), name='home'),
     path('create/', views.create_post, name='create_post'),
     path('<slug:slug>/', views.view_post, name='view_post'),
-    path('<slug:slug>/edit/',
-         views.edit_post, name='edit_post'),
+    path('<slug:slug>/edit/', views.edit_post, name='edit_post'),
+    path('<slug:slug>/delete/', views.delete_post, name='delete_post'),
 ]
