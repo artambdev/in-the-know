03b990a1685701d2af41c8b669d1827c91307b96
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-11 18:04:13 +0000

    Add url pattern for post creation view

--- list/urls.py
+++ list/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.MainPage.as_view(), name='home'),
     path('<slug:slug>/', views.view_post, name='view_post'),
+    path('create/', views.create_post, name='create_post'),
 ]
