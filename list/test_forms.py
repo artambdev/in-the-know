fe625d354b736a62dcec92afdd1de63b8cf501ce
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-12 19:41:19 +0000

    Add test_forms.py with test for post form invalidity

--- list/test_forms.py
+++ list/test_forms.py
@@ -6,3 +6,7 @@ class TestPostForm(TestCase):
     def test_form_is_valid(self):
         post_form = PostForm({'content': 'Hi everyone! 1 plus 1 equals 2!'})
         self.assertTrue(post_form.is_valid(), msg='Post form is not valid')
+
+    def test_form_is_invalid(self):
+        post_form = PostForm({'content': ''})
+        self.assertFalse(post_form.is_valid(), msg='Post form is valid and should not be')
