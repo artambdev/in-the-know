7b2ffd522fb3b69d4ca51728839ed998d7e436eb
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-12 20:02:25 +0000

    Add test for deletion of posts

--- list/test_views.py
+++ list/test_views.py
@@ -18,6 +18,9 @@ class TestBlogViews(TestCase):
         self.post.save()
 
     def test_render_post_detail_page_with_reply_form(self):
+        """
+        Test that a post can be viewed directly
+        """
         response = self.client.get(reverse(
             'view_post', args=['post-name']))
         self.assertEqual(response.status_code, 200)
@@ -26,6 +29,9 @@ class TestBlogViews(TestCase):
             response.context['post_form'], PostForm)
     
     def test_successful_post_submission(self):
+        """
+        Test that a post can be successfully created
+        """
         self.client.login(
             username="userName", password="passWord")
         post_data = {
@@ -38,3 +44,17 @@ class TestBlogViews(TestCase):
             b'This is a test post!',
             response.content
         )
+    
+    def test_successful_post_deletion(self):
+        """
+        Test that a post can be deleted
+        And that the post no longer exists
+        """
+        self.client.login(
+            username="userName", password="passWord")
+        response = self.client.post(reverse(
+            'delete_post', args=["post-name"]), follow=True)
+        response2 = self.client.get(reverse(
+            'view_post', args=['post-name']))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response2.status_code, 404)
