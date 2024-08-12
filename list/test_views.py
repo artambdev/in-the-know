1ebc72c44934f0ae595f47f1ab9fd6e902db9e8f
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-12 19:57:18 +0000

    Add tests for post detail page and post submission

--- list/test_views.py
+++ list/test_views.py
@@ -16,3 +16,25 @@ class TestBlogViews(TestCase):
                          slug="post-name", content="Post content",
                          hidden=False)
         self.post.save()
+
+    def test_render_post_detail_page_with_reply_form(self):
+        response = self.client.get(reverse(
+            'view_post', args=['post-name']))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Post content", response.content)
+        self.assertIsInstance(
+            response.context['post_form'], PostForm)
+    
+    def test_successful_post_submission(self):
+        self.client.login(
+            username="userName", password="passWord")
+        post_data = {
+            'content': 'This is a test post!'
+        }
+        response = self.client.post(reverse(
+            'create_post'), post_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'This is a test post!',
+            response.content
+        )
