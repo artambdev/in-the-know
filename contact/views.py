8d3fc976f7190a22ea5991f5e855b92b6773d470
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-13 17:33:41 +0000

    Create view for contact page

--- contact/views.py
+++ contact/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render
+from .forms import FeedbackForm
+
+def about_me(request):
+    feedback_form = FeedbackForm()
+
+    return render(
+        request,
+        "contact/contact_page.html",
+        {
+            "feedback_form": feedback_form
+        },
+    )
