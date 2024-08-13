587718da3afed746735f4c7953698ebb35588454
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-13 17:30:08 +0000

    Create model for sent feedback

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Feedback(models.Model):
+    """
+    Class for feedback sent to admins
+    """
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    resolved = models.BooleanField(default=False)
+    sent_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['sent_on']
+
+    def __str__(self):
+        return f"Feedback submitted by {self.name}"
