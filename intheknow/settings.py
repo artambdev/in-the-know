610d6fec2e55dfe201e8b3f24da07033d3e464e1
Arthur Ambalov <arthurambalov@gmail.com>
2024-08-18 14:57:16 +0000

    Disable debug flag

--- intheknow/settings.py
+++ intheknow/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-artambdev-intheknow-6omw50yl6q2.ws-eu115.gitpod.io', '.herokuapp.com']
 
